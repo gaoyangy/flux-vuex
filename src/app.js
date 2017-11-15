74aa53a5dcae0915e4164d58978ead0516057753
gaoyangy <13373935140@163.com>
2017-11-15 13:52:19 +0800

    remove public

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from './store/todo'
 import routerMap from './router/router'
+import 'babel-polyfill'
 import App from './Todo.vue'
 // import actions from './store/actions'
 // import mutations from './store/mutations'
 // import getters from './store/getters'
 // import state from './store/state'
-
+Vue.config.devtools = true
 Vue.use(VueRouter)
 
 let router = new VueRouter({
